ec01972406e55fce83d9af9876fd5e058bf5256e
Michael Jones <mwj2g19@soton.ac.uk>
2023-09-02 13:53:35 +0100

    db config

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 from peewee import *
+from credentials import *
 
 #CONFIGURE DATABASE HERE
-db = SqliteDatabase('data.db')
+#db = SqliteDatabase('data.db')
+db = MySQLDatabase('icetracker', user=USERNAME, password=PASSWORD, host='localhost', port=3306)
